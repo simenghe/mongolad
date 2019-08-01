dbdf09d569d25de73bb0db2097c931eb7f013c2d
simeng.he <simeng.he@plasticmobile.com>
2019-08-01 14:45:01 -0400

    firsty

---
+++ src/index.js
@@ -0,0 +1,37 @@
+import { ApolloServer, gql } from 'apollo-server-express';
+//import  { typeDefs, resolvers } from './schema';
+import express from 'express';
+import mongoose from 'mongoose';
+import { typeDefs } from './typeDefs';
+import { resolvers } from './resolvers';
+
+const url = `mongodb://127.0.0.1:27017/nodetest1`;
+
+mongoose.connect(url, { useNewUrlParser: true });
+const startServer = async () => {
+    const app = express();
+
+    const server = new ApolloServer({
+        // These will be defined for both new or existing servers
+        typeDefs,
+        resolvers,
+    });
+
+    server.applyMiddleware({ app }); // app is from an existing express app
+    await mongoose.connect(url, {
+        useNewUrlParser: true
+    });
+    app.listen({ port: 4000 }, () =>
+        console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+    )
+};
+startServer();
+
+// import mongoose from 'mongoose'
+// mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true });
+
+// const Cat = mongoose.model('Cat', { name: String });
+
+// const kitty = new Cat({ name: 'Zildjian' });
+// kitty.save().then(() => console.log('meow'));
+
